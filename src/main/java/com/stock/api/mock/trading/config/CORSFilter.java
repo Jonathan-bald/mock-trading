ef5daa181825f13fd7a1562f2916cc4dd187869a
baldyj117 <baldyj117@yahoo.com>
2019-03-30 14:40:05 -0500

    Adding CORS

---
+++ src/main/java/com/stock/api/mock/trading/config/CORSFilter.java
@@ -0,0 +1,47 @@
+package com.stock.api.mock.trading.config;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class CORSFilter implements Filter {
+
+    public CORSFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        // Authorize (allow) all domains to consume the content
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
+        if (request.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+        // pass the request along the filter chain
+        chain.doFilter(request, servletResponse);
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+}
